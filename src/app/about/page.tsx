35eaf194beef1eeb286544ce509fe1c2fb22b0ab
Adham Khalifa <adhamkhalifa94@gmail.com>
2023-11-06 18:01:40 +0200

    test

---
+++ src/app/about/page.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+async function getData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+const About = async () => {
+  const res = await getData();
+  console.log("res in about", res);
+  return <div>body:{res?.title}</div>;
+};
+
+export default About;
